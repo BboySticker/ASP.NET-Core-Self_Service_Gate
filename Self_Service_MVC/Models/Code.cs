ca4f9489b3da1a5cd371cc546985216e813d9043
Sticker <bboyzxy@gmail.com>
2019-03-13 14:40:31 +0800

    修复url问题

---
+++ Self_Service_MVC/Models/Code.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Self_Service_MVC.Models
+{
+    public class Code
+    {
+        public int MobileCode { get; set; }
+        public int OfficalCode { get; set; }
+    }
+}
